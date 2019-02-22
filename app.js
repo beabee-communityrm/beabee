ff5e6342dec62b202645348017c0afd355a0ff94
Will Franklin <will@willpf.co.uk>
2019-02-22 17:50:19 +0000

    Switch to generated script

--- app.js
+++ app.js
@@ -61,7 +61,11 @@ app.set( 'view cache', false );
 app_loader( app );
 
 // Setup tracker
-app.use( '/membership.js', express.static( __root + '/src/membership.js' ) );
+app.use( '/membership.js', (req, res) => {
+	const memberId = req.cookies.memberId;
+	res.set('Content-Type', 'application/javascript');
+	res.send('window.Membership = {memberId: "' + memberId + '"}');
+});
 
 // Error 404
 app.use( function ( req, res, next ) { // eslint-disable-line no-unused-vars
