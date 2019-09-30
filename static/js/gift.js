5a302639f3d8f8c614dd281d55770006c253a82f
Will Franklin <will@willpf.co.uk>
2019-09-30 16:14:24 +0100

    Add date input fallback and hide flow for No JS

--- static/js/gift.js
+++ static/js/gift.js
@@ -1,16 +1,45 @@
-/* global $, stripe */
+/* global $, document, stripe */
+
+// https://stackoverflow.com/questions/10193294/how-can-i-tell-if-a-browser-supports-input-type-date
+function checkDateInput() {
+	var input = document.createElement('input');
+	input.setAttribute('type','date');
+
+	var notADateValue = 'not-a-date';
+	input.setAttribute('value', notADateValue); 
+
+	return input.value !== notADateValue;
+}
 
 (function () {
 	var $form = $('.js-gift-form');
 	var $errors = $('.js-gift-errors');
+	var $startDate = $form.find('[name=startDate]');
+
+	if (!checkDateInput()) {
+		$startDate.attr({
+			type: 'text',
+			placeholder: 'dd/mm/yyyy',
+			pattern: '[0-3]\\d/[01]\\d/\\d{4}'
+		});
+	}
 
 	$form.on('submit', function (evt) {
 		evt.preventDefault();
 
+		var data;
+		if (!checkDateInput()) {
+			$startDate.prop('disabled', true);
+			data = $form.serialize() + '&startDate=' + $startDate.val().split('/').reverse().join('-');
+			$startDate.prop('disabled', false);
+		} else {
+			data = $form.serialize();
+		}
+
 		$.ajax({
 			url: '/gift',
 			type: 'POST',
-			data: $form.serialize(),
+			data: data,
 			success: function (data) {
 				stripe.redirectToCheckout({
 					sessionId: data.sessionId
