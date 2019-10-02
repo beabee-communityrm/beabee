d112456c0ed562dde55dee59af431fa85e1abef0
Will Franklin <will@willpf.co.uk>
2019-10-02 17:30:25 +0100

    Stop gifts being bought in the past

--- apps/gift/app.js
+++ apps/gift/app.js
@@ -1,11 +1,13 @@
 const express = require( 'express' );
-const stripe = require( __js + '/stripe' );
+const moment = require( 'moment' );
 
 const config = require( __config );
 
 const { GiftFlows } = require( __js + '/database' );
 const { hasModel, hasSchema } = require( __js + '/middleware' );
+const stripe = require( __js + '/stripe' );
 const { wrapAsync } = require( __js + '/utils' );
+const Options = require( __js + '/options' )();
 
 const { createGiftSchema } = require( './schema.json' );
 
@@ -25,23 +27,27 @@ app.get( '/', ( req, res ) => {
 } );
 
 app.post( '/', hasSchema( createGiftSchema ).orReplyWithJSON, wrapAsync( async ( req, res ) => {
-	const gift = await GiftFlows.create({sessionId: 'UNKNOWN', giftForm: req.body});
+	if (moment(req.body.startDate).endOf('day').isBefore()) {
+		res.status(400).send([Options.getText('flash-gifts-date-in-the-past')]);
+	} else {
+		const gift = await GiftFlows.create({sessionId: 'UNKNOWN', giftForm: req.body});
 
-	const session = await stripe.checkout.sessions.create({
-		success_url: config.audience + '/gift/thanks/' + gift._id,
-		cancel_url: config.audience + '/gift',
-		payment_method_types: ['card'],
-		line_items: [{
-			name: 'Gift membership',
-			amount: 3600,
-			currency: 'gbp',
-			quantity: 1
-		}]
-	});
+		const session = await stripe.checkout.sessions.create({
+			success_url: config.audience + '/gift/thanks/' + gift._id,
+			cancel_url: config.audience + '/gift',
+			payment_method_types: ['card'],
+			line_items: [{
+				name: 'Gift membership',
+				amount: 3600,
+				currency: 'gbp',
+				quantity: 1
+			}]
+		});
 
-	await gift.update({sessionId: session.id});
+		await gift.update({sessionId: session.id});
 
-	res.send({sessionId: session.id});
+		res.send({sessionId: session.id});
+	}
 } ) );
 
 app.get( '/thanks/:_id', hasModel(GiftFlows, '_id'),  ( req, res ) => {
