f447fa67a93e83bbe6c8b58fe501ac8590a21f8f
Will Franklin <will@willpf.co.uk>
2019-10-02 10:53:15 +0100

    Add a few more UI hints to help with voting

--- static/js/poll-directors2019.js
+++ static/js/poll-directors2019.js
@@ -3,11 +3,27 @@
 (function () {
 	var $candidates = $('.js-poll-directors2019-candidate');
 	var $submit = $('.js-poll-directors2019-submit');
+	var $selected = $('.js-poll-directors2019-selected');
 
 	function update() {
-		var votes = $candidates.filter(':checked').length;
-		$submit.prop('disabled', votes > 3);
-		$candidates.filter(':not(:checked)').prop('disabled', votes >= 3);
+		var selectedCandidates = $candidates.filter(':checked').map(function () {
+			return this.value;
+		}).toArray();
+
+		$submit.prop('disabled', selectedCandidates.length > 3);
+		$candidates.filter(':not(:checked)').prop('disabled', selectedCandidates.length >= 3);
+
+		var selectedHTML;
+		if (selectedCandidates.length === 0) {
+			selectedHTML = '<b>no candidates</b>';
+		} else {
+			selectedHTML = '<b>' + selectedCandidates.slice(0, -1).join('</b>, <b>') + '</b>';
+			if (selectedCandidates.length > 1) {
+				selectedHTML += ' and ';
+			}
+			selectedHTML += '<b>' + selectedCandidates.slice(-1)[0] + '</b>';
+		}
+		$selected.html('You have selected ' + selectedHTML + '.');
 	}
 
 	$candidates.on('input', update);
