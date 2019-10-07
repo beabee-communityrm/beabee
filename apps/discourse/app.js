a894a564fd21619a6282b12db48b170425b26cc1
Will Franklin <will@willpf.co.uk>
2019-10-07 12:49:59 +0100

    Check params exist

--- apps/discourse/app.js
+++ apps/discourse/app.js
@@ -12,16 +12,16 @@ const sso = new DiscourseSSO( config.discourse.sso_secret );
 app.get( '/sso', auth.isLoggedIn, async ( req, res ) => {
 	const { sso: payload, sig } = req.query;
 
-	if (sso.validate(payload, sig)) {
+	if (payload && sig && sso.validate(payload, sig)) {
 		const nonce = sso.getNonce(payload);
-		const params = {
+		const loginPayload = {
 			nonce,
 			email: req.user.email,
 			external_id: req.user.uuid,
 			name: req.user.fullname,
 			username: req.user.email
 		};
-		const q = sso.buildLoginString(params);
+		const q = sso.buildLoginString(loginPayload);
 		res.redirect(`${config.discourse.url}/session/sso_login?${q}`);
 	} else {
 		res.status(403).send({error: 'Invalid signature'});
