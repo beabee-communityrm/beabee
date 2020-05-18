37732783bec1b114f213e10dfb42e8e2ac7969c2
Will Franklin <will@willpf.co.uk>
2020-05-18 12:57:14 +0100

    Add GC export

--- apps/tools/apps/exports/exports/index.js
+++ apps/tools/apps/exports/exports/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	'active-members': require('./activeMembers'),
 	'churn-rate': require('./churnRate'),
 	'edition': require('./edition'),
+	'gocardless': require('./gocardless'),
 	'join-reasons': require('./joinReasons'),
 	'poll-answers': require('./pollAnswers'),
 	'poll-letter': require('./pollLetter'),
