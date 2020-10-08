745780ba4f9440ddf144b7af76e79b0e2e59f0e8
Will Franklin <will@willpf.co.uk>
2020-10-08 11:19:40 +0100

    Use created date instead

--- apps/tools/apps/exports/exports/pollAnswers.js
+++ apps/tools/apps/exports/exports/pollAnswers.js
@@ -28,7 +28,7 @@ async function getExport(pollAnswers) {
 			Surname: member.lastname,
 			'Full name': member.fullname,
 			'Email address': member.email,
-			'Date': pollAnswer.updatedAt,
+			'Date': pollAnswer.createdAt,
 			'Answer': pollAnswer.answer,
 			...flat(pollAnswer.additionalAnswers)
 		};
