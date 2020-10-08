08aed861f89ef85472345b43e60f7ed06af7779d
Will Franklin <will@willpf.co.uk>
2020-10-08 15:33:39 +0100

    Make special URL action repeater more general

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 /* global $, document */
 
-// Reveal with form input disabler
 (function () {
+	// Reveal with form input disabler
 	$(document.body).on('input', '.js-reveal-types', function () {
 		var $revealTypes = $(this);
 		var $parent = $revealTypes.parents('.js-reveal');
@@ -10,11 +10,34 @@
 		var currentType = $revealTypes.val();
 
 		$revealType.each(function () {
-			var hidden = this.getAttribute('data-type') !== currentType;
+			var hidden = this.getAttribute('data-type').split('|').indexOf(currentType) === -1;
 			$(this).toggleClass('hidden', hidden);
 			$(this).find('input, select, textarea').prop('disabled', hidden);
 		});
 	});
 
 	$('.js-reveal-types').trigger('input');
+
+	// Repeater
+	var $repeater = $('.js-repeater');
+	var repeaterTemplate = $('.js-repeater-template').html();
+	var repeaterName = $repeater.data('name');
+
+	function updateInputNames() {
+		$repeater.find('li').each(function (itemNo) {
+			$(this).find('[name^=' + repeaterName + ']').each(function () {
+				this.name = this.name.replace(/\[\d+\]/, '[' + itemNo + ']');
+			});
+		});
+	}
+
+	$('.js-repeater-add-item').on('click', () => {
+		$repeater.append($(repeaterTemplate));
+		updateInputNames();
+	});
+
+	$repeater.on('click', '.js-repeater-remove-item', function () {
+		$(this).parents('.js-repeater-item').remove();
+		updateInputNames();
+	});
 })();
