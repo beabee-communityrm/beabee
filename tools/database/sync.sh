2e12fde18f5668844d357fcd1719a7d05b4e82cd
Will Franklin <will@willpf.co.uk>
2020-12-22 18:24:40 +0000

    Update sync script

--- tools/database/sync.sh
+++ tools/database/sync.sh
@@ -2,10 +2,10 @@
 
 cd "$( dirname "$0" )"
 
-export TYPEORM_TYPE=$(jq -r .db.type ../../config/config.json)
+export TYPEORM_CONNECTION=$(jq -r .db.type ../../config/config.json)
 export TYPEORM_USERNAME=$(jq -r .db.username ../../config/config.json)
 export TYPEORM_PASSWORD=$(jq -r .db.password ../../config/config.json)
 export TYPEORM_HOST=$(jq -r .db.host ../../config/config.json)
 export TYPEORM_DATABASE=$(jq -r .db.database ../../config/config.json)
 
-npx typeorm schema:log
+npx typeorm schema:sync
