a71c03e4e64aa8dde10bc4218647f6aac6fd4ec7
Will Franklin <will@willpf.co.uk>
2021-01-06 18:28:27 +0000

    Add ident shorthand for direct copying

--- src/tools/database/migrate.ts
+++ src/tools/database/migrate.ts
@@ -42,33 +42,33 @@ function objectId(field: string) {
 	return (doc: Document) => doc[field] ? doc[field].toString() : null;
 }
 
+function ident<T extends readonly string[]>(fields: T): {[key in T[number]]: any} {
+	return Object.assign(
+		{},
+		...fields.map(field => ({[field]: copy(field)}))
+	);
+}
+
 const migrations: Migration<any>[] = [
 	createMigration(PageSettings, 'pagesettings', {
-		pattern: copy('pattern'),
-		shareUrl: copy('shareUrl'),
-		shareTitle: copy('shareTitle'),
-		shareDescription: copy('shareDescription'),
-		shareImage: copy('shareImage')
+		...ident(['pattern', 'shareUrl', 'shareTitle', 'shareDescription', 'shareImage'] as const),
 	}),
 	createMigration(Payment, 'payments', {
+		...ident(['status', 'description', 'amount'] as const),
 		paymentId: copy('payment_id'),
 		subscriptionId: copy('subscription_id'),
 		subscriptionPeriod: copy('subscription_period'),
 		memberId: objectId('member'),
-		status: copy('status'),
-		description: copy('description'),
-		amount: copy('amount'),
 		amountRefunded: copy('amount_refunded'),
 		createdAt: copy('created'),
 		chargeDate: copy('charge_date'),
 		updatedAt: copy('updated_at')
 	}),
 	createMigration(Option, 'options', {
-		key: copy('key'),
-		value: copy('value')
+		...ident(['key', 'value'] as const)
 	}),
 	createMigration(GiftFlow, 'giftflows', {
-		sessionId: copy('sessionId'),
+		...ident(['sessionId', 'setupCode', 'date', 'completed', 'processed'] as const),
 		giftForm: doc => {
 			const giftForm = new GiftForm();
 			giftForm.firstname = doc.giftForm.firstname;
@@ -86,11 +86,6 @@ const migrations: Migration<any>[] = [
 				giftForm.deliveryAddress = doc.giftForm.delivery_copies_address;
 			}
 			return giftForm;
-		},
-		setupCode: copy('setupCode'),
-		date: copy('date'),
-		completed: copy('completed'),
-		processed: copy('processed')
 	})
 ];
 
