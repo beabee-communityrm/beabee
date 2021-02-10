c5b695b7e76f3b0ff30c1f2467132f1da114d1b3
Will Franklin <will@willpf.co.uk>
2021-02-10 15:36:26 +0000

    Add easy login as permission type for dev

--- src/apps/login/app.ts
+++ src/apps/login/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import passport from 'passport';
 
-import { Members } from '@core/database';
+import { Members, Permissions } from '@core/database';
 import { isValidNextUrl, getNextParam, loginAndRedirect, wrapAsync } from '@core/utils';
+
+import config from '@config';
 
 const app = express();
 
@@ -16,6 +18,22 @@ app.get( '/' , function( req, res ) {
 		res.render( 'index', { nextParam: getNextParam( nextParam ) } );
 	}
 } );
+
+if (config.dev) {
+	app.get('/as/:permission', wrapAsync( async (req, res) => {
+		const permissions = await Permissions.find();
+		const member = await Members.findOne({permissions: {
+			$elemMatch: {
+				permission: permissions.find(p => p.slug === req.params.permission)
+			}
+		}});
+		if (member) {
+			loginAndRedirect(req, res, member);
+		} else {
+			res.redirect('/login');
+		}
+	}));
+}
 
 app.get( '/:code', wrapAsync( async function( req, res ) {
 	const nextParam = req.query.next as string;
