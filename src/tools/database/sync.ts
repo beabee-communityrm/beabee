b45733ae95be0e9714c36c3349bc559ad3d6494d
Will Franklin <will@willpf.co.uk>
2021-02-16 15:12:16 +0000

    Add session table sync

--- src/tools/database/sync.ts
+++ src/tools/database/sync.ts
@@ -1,12 +1,30 @@
 import 'module-alias/register';
 
+import fs from 'fs';
+import path from 'path';
 import { ConnectionOptions, getConnection } from 'typeorm';
 
 import * as db from '@core/database';
 
 import config from '@config';
 
+const sessionSqlPath = path.join(__dirname, 'session.sql');
+
 db.connect(config.mongo, {...config.db, logging: true} as ConnectionOptions).then(async () => {
-	await getConnection().synchronize();
+	const connection = getConnection();
+
+	await connection.synchronize();
+
+	const res = await connection.query(
+		`SELECT EXISTS (
+			SELECT 1
+			FROM information_schema.tables
+			WHERE table_name='session'
+		)`
+	);
+	if (!res[0].exists) {
+		await connection.query(fs.readFileSync(sessionSqlPath).toString());
+	}
+
 	await db.close();
 });
