b55782a5a6aff39c8d2b446a35075a497ac47370
Will Franklin <will@willpf.co.uk>
2021-02-18 16:53:43 +0000

    Move some global locals to app loader

--- src/core/app-loader.ts
+++ src/core/app-loader.ts
@@ -1,11 +1,20 @@
 import fs from 'fs';
+import dot from 'dot';
 import express from 'express';
+import moment from 'moment';
 
 import config from '@config';
 
 import { log } from '@core/logging';
 import templateLocals from '@core/template-locals';
 import { AppConfigOverride, AppConfigOverrides, AppConfig } from './utils';
+
+let git = '';
+try {
+	git = fs.readFileSync( __dirname + '/../revision.txt' ).toString();
+} catch (e) {
+	git = 'DEV';
+}
 
 async function loadAppConfigs(basePath: string, overrides: AppConfigOverrides = {}): Promise<AppConfig[]> {
 	const appConfigs = fs.readdirSync(basePath)
@@ -53,6 +62,13 @@ async function routeApps(parentApp: express.Express, appConfigs: AppConfig[]) {
 
 		const app = await requireApp(appConfig.appPath);
 		app.locals.basedir = __dirname + '/..';
+		app.locals.git = git;
+		app.locals.audienceBlah = 'blah' + config.audience;
+		app.locals.dev = config.dev;
+		app.locals.currencySymbol = config.currencySymbol;
+		app.locals.moment = moment;
+		app.locals.dot = dot;
+
 		parentApp.use('/' + appConfig.path, (req, res, next) => {
 			res.locals.app = appConfig;
 			// Bit of a hack to pass all params everywhere
