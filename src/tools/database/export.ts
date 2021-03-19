627a02927a16d4ab39fcdad4f8a031cef6f0783c
Will Franklin <will@willpf.co.uk>
2021-03-19 18:13:08 +0000

    Simpler, less memory intensive export/import (hopefully!)

--- src/tools/database/export.ts
+++ src/tools/database/export.ts
@@ -1,17 +1,23 @@
 import 'module-alias/register';
 
-import { EJSON } from 'bson';
+import { ConnectionOptions, createQueryBuilder, getRepository } from 'typeorm';
+
+import * as db from '@core/database';
 
 import config from '@config';
-import * as db from '@core/database';
-import newExportTypes, { Drier, DrierMap, NewModelData } from './newTypes';
-import { ConnectionOptions, getRepository } from 'typeorm';
+
+import driers, { Drier, DrierMap } from './driers';
 
 // Anonymise properties but maintain same mapping to keep links
 const valueMap = new Map<string, unknown>();
 
 function isDrier<T>(propMap: DrierMap<T>[WritableKeysOf<T>]): propMap is Drier<T[WritableKeysOf<T>]> {
 	return 'propMap' in propMap;
+}
+
+// Maps don't stringify well
+function replacer(key: string, value: any): any {
+	return value instanceof Map ? [...value] : value;
 }
 
 function runDrier<T>(item: T, drier: Drier<T>): T {
@@ -22,18 +28,12 @@ function runDrier<T>(item: T, drier: Drier<T>): T {
 		const propMap = drier.propMap[prop];
 		const oldValue = item[prop];
 		if (oldValue && propMap) {
-			// Serialize Maps correctly
-			const mapKey = oldValue instanceof Map ?
-				Array.from(oldValue.entries()).toString() : oldValue + '';
+			const valueKey = JSON.stringify(oldValue, replacer);
 
-			let newValue = isDrier(propMap) ? runDrier(oldValue, propMap) :
-				valueMap.get(mapKey) || propMap(oldValue);
+			const newValue = isDrier(propMap) ? runDrier(oldValue, propMap) :
+				valueMap.get(valueKey) || propMap(oldValue);
 
-			if (newValue instanceof Map) {
-				newValue = Array.from(newValue.entries());
-			}
-
-			valueMap.set(mapKey, newValue);
+			valueMap.set(valueKey, newValue);
 			newItem[prop] = newValue as T[WritableKeysOf<T>];
 		}
 	}
@@ -41,22 +41,35 @@ function runDrier<T>(item: T, drier: Drier<T>): T {
 	return newItem;
 }
 
-async function runNewExport<T>(drier: Drier<T>): Promise<NewModelData<T>> {
-	console.error('Fetching new', drier.modelName);
-	const items = await getRepository(drier.model).find({loadRelationIds: true});
+async function runExport<T>(drier: Drier<T>): Promise<void> {
+	console.error(`Anonymising ${getRepository(drier.model).metadata.tableName}`);
 
-	console.error(`Anonymising ${drier.modelName}, got ${items.length} items`);
-	const newItems = items.map(item => runDrier(item, drier));
-	return {items: newItems, modelName: drier.modelName};
+	for (let i = 0; ; i += 1000) {
+		const items = await createQueryBuilder(drier.model)
+			.loadAllRelationIds().offset(i).limit(1000).getMany();
+
+		if (items.length === 0) {
+			break;
+		}
+
+		const newItems = items.map(item => runDrier(item, drier));
+
+		const [query, params] = createQueryBuilder()
+			.insert().into(drier.model).values(newItems).getQueryAndParameters();
+
+		console.log(query + ';');
+		console.log(JSON.stringify(params, replacer));
+	}
 }
 
 async function main() {
-	const newExportData: NewModelData<any>[] = [];
-	for (const newExportType of newExportTypes) {
-		newExportData.push(await runNewExport(newExportType));
+	for (const drier of driers.slice().reverse()) {
+		console.log(`DELETE FROM "${getRepository(drier.model).metadata.tableName}";`);
+		console.log();
 	}
-
-	console.log(EJSON.stringify({newExportData}));
+	for (const drier of driers) {
+		await runExport(drier);
+	}
 }
 
 db.connect(config.mongo, config.db as ConnectionOptions).then(async () => {
