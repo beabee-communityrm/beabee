ddb4808a724e6e9b559b4bd2214d0fa1ee6e7e1a
Will Franklin <will@willpf.co.uk>
2021-03-25 18:07:18 +0000

    Add default segments

--- src/migrations/1616694307645-AddDefaultSegments.ts
+++ src/migrations/1616694307645-AddDefaultSegments.ts
@@ -11,7 +11,7 @@ export class AddDefaultSegments1616694307645 implements MigrationInterface {
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query('DELETE FROM segment WHERE id IN ($1, $2, $3, $4) CASCADE', [
+		await queryRunner.query('DELETE FROM segment WHERE id IN ($1, $2, $3, $4)', [
 			'81664449-adf2-4c3a-aee2-145a96d67726',
 			'ce1b2919-85c1-4134-8231-df6b860c0ae2',
 			'f66f45bd-d406-45f6-87da-34d2cd55297a',
