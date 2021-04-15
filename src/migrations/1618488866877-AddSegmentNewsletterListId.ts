23395743be9b1f14eda49009c3014bc8e898a06e
Will Franklin <will@willpf.co.uk>
2021-04-15 13:21:54 +0100

    Make segments optional sync with a newsletter list, remove old syncing script

---
+++ src/migrations/1618488866877-AddSegmentNewsletterListId.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddSegmentNewsletterListId1618488866877 implements MigrationInterface {
+    name = 'AddSegmentNewsletterListId1618488866877'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "segment" ADD "newsletterListId" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "segment" DROP COLUMN "newsletterListId"`);
+    }
+
+}
--- src/tools/process-segments.ts
+++ src/tools/process-segments.ts
@@ -13,6 +13,7 @@ import Member from '@models/Member';
 import Segment from '@models/Segment';
 import SegmentOngoingEmail from '@models/SegmentOngoingEmail';
 import SegmentMember from '@models/SegmentMember';
+import NewsletterService from '@core/services/NewsletterService';
 
 const log = mainLogger.child({app: 'process-segments'});
 
@@ -59,7 +60,7 @@ async function processSegment(segment: Segment) {
 	const outgoingEmails = await getRepository(SegmentOngoingEmail).find({where: {segment}});
 
 	// Only fetch old members if we need to
-	const oldMembers = outgoingEmails.some(oe => oe.trigger === 'onLeave') ?
+	const oldMembers = segment.newsletterListId || outgoingEmails.some(oe => oe.trigger === 'onLeave') ?
 		await getRepository(Member).findByIds(oldSegmentMembers.map(sm => sm.member)) : [];
 
 	for (const outgoingEmail of outgoingEmails) {
@@ -71,6 +72,11 @@ async function processSegment(segment: Segment) {
 				membersToRecipients(emailMembers)
 			);
 		}
+	}
+
+	if (segment.newsletterListId) {
+		await NewsletterService.upsertMembers(segment.newsletterListId, newMembers);
+		await NewsletterService.archiveMembers(segment.newsletterListId, oldMembers);
 	}
 }
 
