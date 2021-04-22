ce7f49123a8e5896edfdbb5d466baf60d05ed678
Will Franklin <will@willpf.co.uk>
2021-04-22 17:11:14 +0100

    Start resyncing

--- src/apps/settings/apps/newsletters/app.ts
+++ src/apps/settings/apps/newsletters/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import moment from 'moment';
 
 import { isSuperAdmin } from '@core/middleware';
+import { wrapAsync } from '@core/utils';
+
+import OptionsService from '@core/services/OptionsService';
 
 import config from '@config';
 
@@ -13,5 +17,14 @@ app.use(isSuperAdmin);
 app.get('/', (req, res) => {
 	res.render('index', {provider: config.newsletter.provider});
 });
+
+app.post('/', wrapAsync(async (req, res) => {
+	if (req.body.action === 'resync') {
+		req.flash('success', 'newsletter-resync-started');
+		res.redirect(req.originalUrl);
+
+		await OptionsService.set('newsletter-resync-status', 'Resync started at ' + moment.utc().format('hh:mm'));
+	}
+}));
 
 export default app;
