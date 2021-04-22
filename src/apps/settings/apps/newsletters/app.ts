d78b421cbbce112dbe8a5ee9e560882fc6f3495f
Will Franklin <will@willpf.co.uk>
2021-04-22 15:23:51 +0100

    Show provider on settings page

--- src/apps/settings/apps/newsletters/app.ts
+++ src/apps/settings/apps/newsletters/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { isSuperAdmin } from '@core/middleware';
 
+import config from '@config';
+
 const app = express();
 
 app.set( 'views', __dirname + '/views' );
@@ -9,7 +11,7 @@ app.set( 'views', __dirname + '/views' );
 app.use(isSuperAdmin);
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', {provider: config.newsletter.provider});
 });
 
 export default app;
