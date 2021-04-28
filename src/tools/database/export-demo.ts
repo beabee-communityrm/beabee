1f472f1abdf9f912bc8a135e08c50035bf3f26ee
Will Franklin <will@willpf.co.uk>
2021-04-28 15:51:57 +0100

    Get last two polls

--- src/tools/database/export-demo.ts
+++ src/tools/database/export-demo.ts
@@ -8,11 +8,12 @@ import Member from '@models/Member';
 
 import {
 	Drier, gcPaymentDataDrier, gcPaymentsDrier, memberDrier,
-	memberPermissionDrier, memberProfileDrier, runExport
+	memberPermissionDrier, memberProfileDrier, pollResponsesDrier, pollsDrier,
+	runExport
 } from './driers';
 
 async function main() {
-	for (const drier of [gcPaymentDataDrier, gcPaymentsDrier, memberProfileDrier, memberPermissionDrier, memberDrier] as Drier<any>[]) {
+	for (const drier of [pollResponsesDrier, pollsDrier, gcPaymentDataDrier, gcPaymentsDrier, memberProfileDrier, memberPermissionDrier, memberDrier] as Drier<any>[]) {
 		console.log(`DELETE FROM "${getRepository(drier.model).metadata.tableName}";`);
 		console.log();
 	}
@@ -29,6 +30,7 @@ async function main() {
 	await runExport(memberProfileDrier, qb => qb.where('item.memberId IN (:...ids)', {ids: memberIds}), valueMap);
 	await runExport(gcPaymentsDrier, qb => qb.where('item.memberId IN (:...ids)', {ids: memberIds}).orderBy('id'), valueMap);
 	await runExport(gcPaymentDataDrier, qb => qb.where('item.memberId IN (:...ids)', {ids: memberIds}), valueMap);
+	await runExport(pollsDrier, qb => qb.orderBy({date: 'DESC'}).limit(2), valueMap);
 }
 
 db.connect().then(async () => {
