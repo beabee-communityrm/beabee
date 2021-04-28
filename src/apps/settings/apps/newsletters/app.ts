e966062315224802053ca77472929dfe995cdc9e
Will Franklin <will@willpf.co.uk>
2021-04-28 14:31:39 +0100

    Update basic syncing code

--- src/apps/settings/apps/newsletters/app.ts
+++ src/apps/settings/apps/newsletters/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import moment from 'moment';
-import { createQueryBuilder, getRepository } from 'typeorm';
+import { createQueryBuilder } from 'typeorm';
 
 import { isSuperAdmin } from '@core/middleware';
 import { wrapAsync } from '@core/utils';
 
+import MembersService from '@core/services/MembersService';
 import NewsletterService from '@core/services/NewsletterService';
 import OptionsService from '@core/services/OptionsService';
 
@@ -28,16 +29,21 @@ async function setResyncStatus(message: string) {
 
 app.post('/', wrapAsync(async (req, res) => {
 	if (req.body.action === 'resync') {
+		await setResyncStatus('Uploading contacts to newsletter list');
+
 		req.flash('success', 'newsletter-resync-started');
 		res.redirect(req.originalUrl);
 
 		try {
-			await setResyncStatus('Uploading contacts to newsletter list');
-
 			const members = await createQueryBuilder(Member).limit(100).getMany();
-			await NewsletterService.upsertMembers(members, false);
+			await NewsletterService.upsertMembers(members);
 
 			await setResyncStatus('Importing contacts from newsletter list');
+
+			const newsletterMembers = await NewsletterService.getNewsletterMembers();
+			const missingNewsletterMembers = newsletterMembers.filter(nm => members.every(m => nm.email !== m.email));
+
+			//await MembersService.createMember({}, {})
 
 			await OptionsService.reset('newsletter-resync-status');
 		} catch (err) {
