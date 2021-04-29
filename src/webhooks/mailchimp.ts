3b033abfe67cd7e4a4c08463990ac2c51098d274
Will Franklin <will@willpf.co.uk>
2021-04-29 09:36:26 +0100

    Add explanation for GET endpoint

--- src/webhooks/mailchimp.ts
+++ src/webhooks/mailchimp.ts
@@ -52,6 +52,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mailchimp pings this endpoint when you first add the webhook
 app.get('/', (req, res) => {
 	res.sendStatus(200);
 });
