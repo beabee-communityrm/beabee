442e51e8c4f8bd4080044b377794c120153312e8
Will Franklin <will@willpf.co.uk>
2021-05-05 15:36:53 +0100

    Add migration

---
+++ src/migrations/1620225385309-DeliveryOptInDefault.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class DeliveryOptInDefault1620225385309 implements MigrationInterface {
+    name = 'DeliveryOptInDefault1620225385309'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "member_profile"."deliveryOptIn" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "member_profile" ALTER COLUMN "deliveryOptIn" SET DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "member_profile" ALTER COLUMN "deliveryOptIn" DROP DEFAULT`);
+        await queryRunner.query(`COMMENT ON COLUMN "member_profile"."deliveryOptIn" IS NULL`);
+    }
+
+}
