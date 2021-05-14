3cd84bd317c52df48e191e2eac7fe5fb01cdb3e7
Will Franklin <will@willpf.co.uk>
2021-05-14 14:46:30 +0100

    Bring back basic exports page

--- src/apps/members/apps/member/apps/exports/app.ts
+++ src/apps/members/apps/member/apps/exports/app.ts
@@ -1,7 +1,32 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+
+import { wrapAsync } from '@core/utils';
+
+import ExportTypes from '@apps/tools/apps/exports/exports';
+
+import ExportItem from '@models/ExportItem';
+import Member from '@models/Member';
 
 const app = express();
 
 app.set( 'views', __dirname + '/views' );
+
+app.get('/', wrapAsync(async (req, res) => {
+	const member = req.model as Member;
+	const exportItems = await getRepository(ExportItem).find({
+		where: {itemId: member.id},
+		relations: ['export']
+	});
+
+	const exportItemsWithTypes = exportItems
+		.filter(item => !!ExportTypes[item.export.type])
+		.map(item => ({
+			...item,
+			type: new ExportTypes[item.export.type]()
+		}));
+
+	res.render('index', {exportItems: exportItemsWithTypes, member});
+}));
 
 export default app;
