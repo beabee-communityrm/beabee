e9e4b901d229501296910ddebdc4a7671f29a71f
Will Franklin <will@willpf.co.uk>
2021-07-08 16:49:44 +0100

    Return profile with getMe

--- src/api/controllers/MemberController.ts
+++ src/api/controllers/MemberController.ts
@@ -1,10 +1,17 @@
-import Member from '@models/Member';
 import { CurrentUser, Get, JsonController } from 'routing-controllers';
+import { getRepository } from 'typeorm';
+
+import Member from '@models/Member';
+import MemberProfile from '@models/MemberProfile';
 
 @JsonController('/member')
 export class MemberController {
 	@Get('/me')
-	me(@CurrentUser({required: true}) member: Member): Member {
+	async getMe(@CurrentUser({required: true}) member: Member): Promise<Member> {
+		const profile = await getRepository(MemberProfile).findOne({member});
+		if (profile) {
+			member.profile = profile;
+		}
 		return member;
 	}
 }
