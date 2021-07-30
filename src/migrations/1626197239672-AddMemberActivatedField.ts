7d8ecb1b4d06d8559124d744ff1b2f384ad9424a
Will Franklin <will@willpf.co.uk>
2021-07-30 12:46:06 +0100

    Fix typo in migration

--- src/migrations/1626197239672-AddMemberActivatedField.ts
+++ src/migrations/1626197239672-AddMemberActivatedField.ts
@@ -6,7 +6,7 @@ export class AddMemberActivatedField1626197239672 implements MigrationInterface 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "member" ADD "activated" boolean NOT NULL DEFAULT false`);
         // Prior to activated existing everyone was activated
-        await queryRunner.query(`UPDATE "member" SET "activated=true`);
+        await queryRunner.query(`UPDATE "member" SET "activated"=true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
