78932eb0334b4d9807cf5587c3b1213419b1f8cf
Will Franklin <will@willpf.co.uk>
2021-09-16 15:51:39 +0100

    Make a few things required

--- src/api/controllers/SignupController.ts
+++ src/api/controllers/SignupController.ts
@@ -95,7 +95,9 @@ async function handleJoin(
 @JsonController("/signup")
 export class SignupController {
   @Post("/")
-  async startSignup(@Body() data: SignupData): Promise<SignupStart> {
+  async startSignup(
+    @Body({ required: true }) data: SignupData
+  ): Promise<SignupStart> {
     const redirectUrl = await JoinFlowService.createJoinFlow(
       data.completeUrl,
       {
@@ -122,7 +124,7 @@ export class SignupController {
   @Post("/complete")
   async completeSignup(
     @Req() req: Request,
-    @BodyParam("redirectFlowId") redirectFlowId: string
+    @BodyParam("redirectFlowId", { required: true }) redirectFlowId: string
   ): Promise<void> {
     const joinFlow = await JoinFlowService.completeJoinFlow(redirectFlowId);
     if (!joinFlow) {
@@ -174,7 +176,7 @@ export class SignupController {
   @Post("/confirm-email")
   async confirmEmail(
     @Req() req: Request,
-    @BodyParam("restartFlowId") restartFlowId: string
+    @BodyParam("restartFlowId", { required: true }) restartFlowId: string
   ): Promise<void> {
     const restartFlow = await JoinFlowService.completeRestartFlow(
       restartFlowId
