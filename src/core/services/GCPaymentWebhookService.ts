1b26a5c0a6f36ab4e16ce4cc9cc5434a9aeb5873
Will Franklin <will@willpf.co.uk>
2021-09-20 15:27:12 +0100

    Add note to remember to fix reactivating members (after failed payment)

--- src/core/services/GCPaymentWebhookService.ts
+++ src/core/services/GCPaymentWebhookService.ts
@@ -85,6 +85,7 @@ export default class GCPaymentWebhookService {
       "member",
       nextExpiryDate.toDate()
     );
+    // TODO: resubscribe to newsletter
   }
 
   static async updatePaymentStatus(
