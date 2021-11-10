11326356951c6cb9e72636574821103869bc1c9b
Will Franklin <will@willpf.co.uk>
2021-11-10 16:30:19 +0000

    feat: update payment source endpoints

--- src/api/controllers/MemberController.ts
+++ src/api/controllers/MemberController.ts
@@ -14,12 +14,17 @@ import {
   CurrentUser,
   Get,
   JsonController,
+  NotFoundError,
+  OnUndefined,
+  Post,
   Put
 } from "routing-controllers";
 import { getRepository } from "typeorm";
 
 import { NewsletterStatus } from "@core/providers/newsletter";
 
+import GCPaymentService from "@core/services/GCPaymentService";
+import JoinFlowService from "@core/services/JoinFlowService";
 import MembersService from "@core/services/MembersService";
 import PaymentService, { PaymentSource } from "@core/services/PaymentService";
 
@@ -27,10 +32,15 @@ import { ContributionPeriod } from "@core/utils";
 import { generatePassword } from "@core/utils/auth";
 
 import Address from "@models/Address";
+import JoinFlow from "@models/JoinFlow";
 import Member from "@models/Member";
 import MemberProfile from "@models/MemberProfile";
 
 import config from "@config";
+
+import IsUrl from "@api/validators/IsUrl";
+import DuplicateEmailError from "@api/errors/DuplicateEmailError";
+import CantUpdateContribution from "@api/errors/CantUpdateContribution";
 
 interface MemberData {
   email: string;
@@ -99,6 +109,16 @@ class UpdateMemberData implements Partial<MemberData> {
   profile?: UpdateMemberProfileData;
 }
 
+class UpsertPaymentSourceData {
+  @IsUrl()
+  completeUrl!: string;
+}
+
+class UpsertPaymentSourceCompleteData {
+  @IsString()
+  redirectFlowId!: string;
+}
+
 async function memberToApiMember(member: Member): Promise<GetMemberData> {
   const profile = await getRepository(MemberProfile).findOneOrFail({ member });
 
@@ -160,5 +180,57 @@ export class MemberController {
     @CurrentUser({ required: true }) member: Member
   ): Promise<PaymentSource | undefined> {
     return await PaymentService.getPaymentSource(member);
+  }
+
+  @Put("/member/me/payment-source")
+  async setPaymentSource(
+    @CurrentUser({ required: true }) member: Member,
+    @Body({ required: true }) data: UpsertPaymentSourceData
+  ): Promise<{ redirectUrl: string }> {
+    const redirectUrl = await JoinFlowService.createJoinFlow(
+      data.completeUrl,
+      // TODO: this is all unnecessary, we should remove this
+      {
+        monthlyAmount: 0,
+        period: ContributionPeriod.Monthly,
+        password: await generatePassword(""),
+        email: "",
+        payFee: true,
+        prorate: false
+      },
+      {
+        prefilled_customer: {
+          email: member.email,
+          given_name: member.firstname,
+          family_name: member.lastname
+        }
+      }
+    );
+    return {
+      redirectUrl
+    };
+  }
+
+  @OnUndefined(204)
+  @Post("/member/me/payment-source/complete")
+  async completePaymentSource(
+    @CurrentUser({ required: true }) member: Member,
+    @Body({ required: true }) data: UpsertPaymentSourceCompleteData
+  ): Promise<void> {
+    const joinFlow = await getRepository(JoinFlow).findOne({
+      redirectFlowId: data.redirectFlowId
+    });
+    if (!joinFlow) {
+      throw new NotFoundError();
+    }
+
+    const { customerId, mandateId } = await JoinFlowService.completeJoinFlow(
+      joinFlow
+    );
+    if (await GCPaymentService.canChangeContribution(member, false)) {
+      await GCPaymentService.updatePaymentMethod(member, customerId, mandateId);
+    } else {
+      throw new CantUpdateContribution();
+    }
   }
 }
