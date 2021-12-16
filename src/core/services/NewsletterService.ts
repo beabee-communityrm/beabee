0458720b0d7248eddf1dce92bd9cf3a5f44635ef
Will Franklin <will@willpf.co.uk>
2021-12-16 14:22:12 +0000

    chore: ensure profile is always loaded for updates

--- src/core/services/NewsletterService.ts
+++ src/core/services/NewsletterService.ts
@@ -29,29 +29,45 @@ function shouldUpdate(updates: Partial<Member>): boolean {
   );
 }
 
-function memberToNlUpdate(member: Member): UpdateNewsletterMember {
-  return {
-    email: member.email,
-    status: member.profile.newsletterStatus,
-    groups: member.profile.newsletterGroups,
-    firstname: member.firstname,
-    lastname: member.lastname,
-    fields: {
-      REFCODE: member.referralCode || "",
-      POLLSCODE: member.pollsCode || "",
-      C_DESC: member.contributionDescription,
-      C_MNTHAMT: member.contributionMonthlyAmount?.toString() || "",
-      C_PERIOD: member.contributionPeriod || ""
+async function memberToNlUpdate(
+  member: Member
+): Promise<UpdateNewsletterMember | undefined> {
+  // TODO: Fix that it relies on member.profile being loaded
+  if (!member.profile) {
+    member.profile = await getRepository(MemberProfile).findOneOrFail({
+      member
+    });
+  }
+
+  if (member.profile.newsletterStatus !== NewsletterStatus.None) {
+    return {
+      email: member.email,
+      status: member.profile.newsletterStatus,
+      groups: member.profile.newsletterGroups,
+      firstname: member.firstname,
+      lastname: member.lastname,
+      fields: {
+        REFCODE: member.referralCode || "",
+        POLLSCODE: member.pollsCode || "",
+        C_DESC: member.contributionDescription,
+        C_MNTHAMT: member.contributionMonthlyAmount?.toString() || "",
+        C_PERIOD: member.contributionPeriod || ""
+      }
+    };
+  }
+}
+
+async function getValidMembers(
+  members: Member[]
+): Promise<UpdateNewsletterMember[]> {
+  const nlMembers = [];
+  for (const member of members) {
+    const nlMember = await memberToNlUpdate(member);
+    if (nlMember) {
+      nlMembers.push(nlMember);
     }
-  };
-}
-
-function isValidMember(member: Member): boolean {
-  return member.profile.newsletterStatus !== NewsletterStatus.None;
-}
-
-function getValidMembers(members: Member[]): UpdateNewsletterMember[] {
-  return members.filter(isValidMember).map(memberToNlUpdate);
+  }
+  return nlMembers;
 }
 
 class NewsletterService {
@@ -63,7 +79,7 @@ class NewsletterService {
   async addTagToMembers(members: Member[], tag: string): Promise<void> {
     log.info(`Add tag ${tag} to ${members.length} members`);
     await this.provider.addTagToMembers(
-      getValidMembers(members).map((m) => m.email),
+      (await getValidMembers(members)).map((m) => m.email),
       tag
     );
   }
@@ -71,7 +87,7 @@ class NewsletterService {
   async removeTagFromMembers(members: Member[], tag: string): Promise<void> {
     log.info(`Remove tag ${tag} from ${members.length} members`);
     await this.provider.removeTagFromMembers(
-      getValidMembers(members).map((m) => m.email),
+      (await getValidMembers(members)).map((m) => m.email),
       tag
     );
   }
@@ -81,43 +97,34 @@ class NewsletterService {
     updates?: Partial<Member>,
     oldEmail?: string
   ): Promise<void> {
-    const willUpdate =
-      isValidMember(member) && (!updates || shouldUpdate(updates));
+    const willUpdate = !updates || shouldUpdate(updates);
 
     if (willUpdate) {
-      log.info("Upsert member " + member.id);
-      // TODO: Fix that it relies on member.profile being loaded
-      if (!member.profile) {
-        member.profile = await getRepository(MemberProfile).findOneOrFail({
-          member
-        });
+      const nlMember = await memberToNlUpdate(member);
+      if (nlMember) {
+        log.info("Upsert member " + member.id);
+        await this.provider.updateMember(nlMember, oldEmail);
+      } else {
+        log.info("Ignoring member update for " + member.id);
       }
-      await this.provider.updateMember(memberToNlUpdate(member), oldEmail);
-    } else {
-      log.info("Ignoring member update for " + member.id);
     }
   }
 
   async upsertMembers(members: Member[]): Promise<void> {
     log.info(`Upsert ${members.length} members`);
-    await this.provider.upsertMembers(getValidMembers(members));
+    await this.provider.upsertMembers(await getValidMembers(members));
   }
 
   async updateMemberFields(
     member: Member,
     fields: Record<string, string>
   ): Promise<void> {
-    if (isValidMember(member)) {
-      log.info(`Update member fields for ${member.id}`, fields);
-      // TODO: Fix that it relies on member.profile being loaded
-      if (!member.profile) {
-        member.profile = await getRepository(MemberProfile).findOneOrFail({
-          member
-        });
-      }
+    log.info(`Update member fields for ${member.id}`, fields);
+    const nlMember = await memberToNlUpdate(member);
+    if (nlMember) {
       await this.provider.updateMember({
-        email: member.email,
-        status: member.profile.newsletterStatus,
+        email: nlMember.email,
+        status: nlMember.status,
         fields
       });
     } else {
@@ -128,14 +135,14 @@ class NewsletterService {
   async archiveMembers(members: Member[]): Promise<void> {
     log.info(`Archive ${members.length} members`);
     await this.provider.archiveMembers(
-      getValidMembers(members).map((m) => m.email)
+      (await getValidMembers(members)).map((m) => m.email)
     );
   }
 
   async deleteMembers(members: Member[]): Promise<void> {
     log.info(`Delete ${members.length} members`);
     await this.provider.deleteMembers(
-      getValidMembers(members).map((m) => m.email)
+      (await getValidMembers(members)).map((m) => m.email)
     );
   }
 
