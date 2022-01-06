cd326b77b097321f9ebfb4283df8a61e4e54cccc
Will Franklin <will@willpf.co.uk>
2022-01-06 10:22:45 +0000

    feat: superadmin implies admin

--- src/api/controllers/MemberController.ts
+++ src/api/controllers/MemberController.ts
@@ -94,6 +94,14 @@ export class MemberController {
           })
         : undefined;
 
+    const roles = target.permissions
+      .filter((p) => p.isActive)
+      .map((p) => p.permission);
+
+    if (roles.includes("superadmin")) {
+      roles.push("admin");
+    }
+
     return {
       email: target.email,
       firstname: target.firstname,
@@ -101,9 +109,7 @@ export class MemberController {
       joined: target.joined,
       contributionAmount: target.contributionAmount,
       contributionPeriod: target.contributionPeriod,
-      roles: target.permissions
-        .filter((p) => p.isActive)
-        .map((p) => p.permission),
+      roles,
       ...(profile && {
         profile: {
           telephone: profile.telephone,
