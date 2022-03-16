2a49e02b44acd672bfd66687fa89ab57abdfbcab
Will Franklin <will@willpf.co.uk>
2022-03-16 12:41:52 +0000

    chore: proper types for subCode

--- src/api/errors/InvalidCalloutResponse.ts
+++ src/api/errors/InvalidCalloutResponse.ts
@@ -1,8 +1,16 @@
 import { BadRequestError } from "routing-controllers";
 
+type InvalidCalloutResponseCode =
+  | "only-anonymous"
+  | "expired-user"
+  | "closed"
+  | "cant-update"
+  | "guest-fields-missing"
+  | "logged-in-guest-fields";
+
 export default class InvalidCalloutResponse extends BadRequestError {
   readonly code = "invalid-callout-response";
-  constructor(readonly subCode: string) {
+  constructor(readonly subCode: InvalidCalloutResponseCode) {
     super();
     Object.setPrototypeOf(this, InvalidCalloutResponse.prototype);
   }
