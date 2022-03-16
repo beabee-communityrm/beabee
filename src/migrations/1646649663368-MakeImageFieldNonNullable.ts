ef52f8702f6963c74d87bda880fc557aba75a9f8
Will Franklin <will@willpf.co.uk>
2022-03-16 15:33:10 +0000

    fix: apply prettier to migration

--- src/migrations/1646649663368-MakeImageFieldNonNullable.ts
+++ src/migrations/1646649663368-MakeImageFieldNonNullable.ts
@@ -1,14 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class MakeImageFieldNonNullable1646649663368 implements MigrationInterface {
-    name = 'MakeImageFieldNonNullable1646649663368'
+export class MakeImageFieldNonNullable1646649663368
+  implements MigrationInterface
+{
+  name = "MakeImageFieldNonNullable1646649663368";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "poll" ALTER COLUMN "image" SET NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "poll" ALTER COLUMN "image" SET NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "poll" ALTER COLUMN "image" DROP NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "poll" ALTER COLUMN "image" DROP NOT NULL`
+    );
+  }
 }
