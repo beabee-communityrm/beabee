e1cdd0acef46fce8d084cca17f9fb957c599ee3a
Will Franklin <will@willpf.co.uk>
2022-09-29 11:47:02 +0100

    feat: use with query pattern for callout API

--- src/api/controllers/CalloutController.ts
+++ src/api/controllers/CalloutController.ts
@@ -12,161 +12,56 @@ import {
   Post,
   QueryParams
 } from "routing-controllers";
-import { createQueryBuilder, getRepository } from "typeorm";
+import { getRepository } from "typeorm";
 
 import PollsService from "@core/services/PollsService";
 
-import ItemStatus, { ruleAsQuery } from "@models/ItemStatus";
 import Member from "@models/Member";
 import Poll from "@models/Poll";
 import PollResponse from "@models/PollResponse";
 
 import {
+  convertCalloutToData,
   CreateCalloutData,
   CreateCalloutResponseData,
-  GetBasicCalloutData,
+  fetchPaginatedCalloutResponses,
+  fetchPaginatedCallouts,
+  GetCalloutData,
+  GetCalloutQuery,
   GetCalloutResponseData,
   GetCalloutResponsesQuery,
   GetCalloutsQuery,
-  GetMoreCalloutData,
   UpdateCalloutData
 } from "@api/data/CalloutData";
 import InvalidCalloutResponse from "@api/errors/InvalidCalloutResponse";
-import { fetchPaginated, mergeRules, Paginated } from "@api/utils/pagination";
+import { Paginated } from "@api/utils/pagination";
 
-function pollToBasicCallout(poll: Poll): GetBasicCalloutData {
-  return {
-    slug: poll.slug,
-    title: poll.title,
-    excerpt: poll.excerpt,
-    image: poll.image,
-    allowUpdate: poll.allowUpdate,
-    allowMultiple: poll.allowMultiple,
-    access: poll.access,
-    status: poll.status,
-    hidden: poll.hidden,
-    starts: poll.starts,
-    expires: poll.expires,
-    ...(poll.hasAnswered !== undefined && {
-      hasAnswered: poll.hasAnswered
-    })
-  };
-}
-
-@JsonController("/callout")
-export class CalloutController {
-  @Get("/")
-  async getCallouts(
-    @CurrentUser() member: Member | undefined,
-    @QueryParams() query: GetCalloutsQuery
-  ): Promise<Paginated<GetBasicCalloutData>> {
-    const scopedQuery = mergeRules(
-      query,
-      !member?.hasPermission("admin") && [
-        // Non-admins can only query for open or ended non-hidden callouts
-        {
-          condition: "OR",
-          rules: [
-            { field: "status", operator: "equal", value: ItemStatus.Open },
-            { field: "status", operator: "equal", value: ItemStatus.Ended }
-          ]
-        },
-        { field: "hidden", operator: "equal", value: false }
-      ]
-    );
-
-    const results = await fetchPaginated(Poll, scopedQuery, {
-      // TODO: add validation errors
-      status: ruleAsQuery,
-      answeredBy: (rule, qb, suffix) => {
-        if (rule.operator !== "equal" || !member) return;
-
-        // TODO: allow admins to filter for other users
-        if (rule.value !== "me" && rule.value !== member.id) return;
-
-        // TODO: deduplicate with hasAnswered
-        const subQb = createQueryBuilder()
-          .subQuery()
-          .select("pr.pollSlug", "slug")
-          .distinctOn(["pr.pollSlug"])
-          .from(PollResponse, "pr")
-          .where(`pr.memberId = :id${suffix}`)
-          .orderBy("pr.pollSlug");
-
-        qb.where("item.slug IN " + subQb.getQuery());
-
-        return {
-          id: member.id
-        };
-      }
-    });
-
-    if (
-      member &&
-      results.items.length > 0 &&
-      (query.hasAnswered === "me" || query.hasAnswered === member.id)
-    ) {
-      const answeredPolls = await createQueryBuilder(PollResponse, "pr")
-        .select("pr.pollSlug", "slug")
-        .distinctOn(["pr.pollSlug"])
-        .where("pr.pollSlug IN (:...slugs) AND pr.memberId = :id", {
-          slugs: results.items.map((item) => item.slug),
-          id: member.id
-        })
-        .orderBy("pr.pollSlug")
-        .getRawMany<{ slug: string }>();
-
-      const answeredSlugs = answeredPolls.map((p) => p.slug);
-
-      for (const item of results.items) {
-        item.hasAnswered = answeredSlugs.includes(item.slug);
-      }
-    }
-
-    return {
-      ...results,
-      items: results.items.map(pollToBasicCallout)
-    };
-  }
-
+abstract class CalloutAdminController {
   @Authorized("admin")
   @Post("/")
   async createCallout(
+    @CurrentUser({ required: true }) member: Member,
     @Body() data: CreateCalloutData
-  ): Promise<GetBasicCalloutData> {
-    const poll = await getRepository(Poll).save(data);
-    return pollToBasicCallout(poll);
-  }
-
-  @Get("/:slug")
-  async getCallout(
-    @Param("slug") slug: string
-  ): Promise<GetMoreCalloutData | undefined> {
-    const poll = await getRepository(Poll).findOne(slug);
-    if (poll) {
-      return {
-        ...pollToBasicCallout(poll),
-        intro: poll.intro,
-        thanksText: poll.thanksText,
-        thanksTitle: poll.thanksTitle,
-        formSchema: poll.formSchema,
-        ...(poll.thanksRedirect && { thanksRedirect: poll.thanksRedirect }),
-        ...(poll.shareTitle && { shareTitle: poll.shareTitle }),
-        ...(poll.shareDescription && {
-          shareDescription: poll.shareDescription
-        })
-      };
+  ): Promise<GetCalloutData> {
+    await getRepository(Poll).insert(data);
+    const poll = await getRepository(Poll).findOne(data.slug);
+    // Should be impossible
+    if (!poll) {
+      throw new Error("Callout just inserted but not found");
     }
+    return convertCalloutToData(poll, member, {});
   }
 
   @Authorized("admin")
   @Patch("/:slug")
   async updateCallout(
+    @CurrentUser({ required: true }) member: Member,
     @Param("slug") slug: string,
     @Body() data: UpdateCalloutData
-  ): Promise<GetMoreCalloutData | undefined> {
+  ): Promise<GetCalloutData | undefined> {
     await getRepository(Poll).update(slug, data);
-    return this.getCallout(slug);
+    const poll = await getRepository(Poll).findOne(slug);
+    return poll && convertCalloutToData(poll, member, {});
   }
 
   @Authorized("admin")
@@ -179,6 +74,29 @@ export class CalloutController {
       throw new NotFoundError();
     }
   }
+}
+
+@JsonController("/callout")
+export class CalloutController extends CalloutAdminController {
+  @Get("/")
+  async getCallouts(
+    @CurrentUser() member: Member | undefined,
+    @QueryParams() query: GetCalloutsQuery
+  ): Promise<Paginated<GetCalloutData>> {
+    return fetchPaginatedCallouts(query, member, { with: query.with });
+  }
+
+  @Get("/:slug")
+  async getCallout(
+    @CurrentUser() member: Member | undefined,
+    @Param("slug") slug: string,
+    @QueryParams() query: GetCalloutQuery
+  ): Promise<GetCalloutData | undefined> {
+    const poll = await getRepository(Poll).findOne(slug);
+    if (poll) {
+      return convertCalloutToData(poll, member, { with: query.with });
+    }
+  }
 
   @Get("/:slug/responses")
   async getCalloutResponses(
@@ -186,38 +104,7 @@ export class CalloutController {
     @Param("slug") slug: string,
     @QueryParams() query: GetCalloutResponsesQuery
   ): Promise<Paginated<GetCalloutResponseData>> {
-    const scopedQuery = mergeRules(query, [
-      { field: "poll", operator: "equal", value: slug },
-      // Member's can only see their own responses
-      !member.hasPermission("admin") && {
-        field: "member",
-        operator: "equal",
-        value: member.id
-      }
-    ]);
-    const results = await fetchPaginated(
-      PollResponse,
-      scopedQuery,
-      {
-        member: (rule, qb, suffix, namedWhere) => {
-          qb.where(`item.member ${namedWhere}`);
-          if (rule.value === "me") {
-            return { a: member.id };
-          }
-        }
-      },
-      (qb) => qb.loadAllRelationIds()
-    );
-
-    return {
-      ...results,
-      items: results.items.map((item) => ({
-        member: item.member as unknown as string, // TODO: fix typing
-        answers: item.answers,
-        createdAt: item.createdAt,
-        updatedAt: item.updatedAt
-      }))
-    };
+    return await fetchPaginatedCalloutResponses(slug, query, member);
   }
 
   @Post("/:slug/responses")
---
+++ src/api/data/CalloutData/index.ts
@@ -0,0 +1,204 @@
+import Poll from "@models/Poll";
+
+import { fetchPaginated, mergeRules, Paginated } from "@api/utils/pagination";
+
+import {
+  GetCalloutWith,
+  GetCalloutsQuery,
+  GetCalloutResponsesQuery,
+  GetCalloutResponseData,
+  GetCalloutData
+} from "./interface";
+import { createQueryBuilder, getRepository } from "typeorm";
+import PollResponse from "@models/PollResponse";
+import ItemStatus, { ruleAsQuery } from "@models/ItemStatus";
+import Member from "@models/Member";
+import { BadRequestError, UnauthorizedError } from "routing-controllers";
+
+interface ConvertOpts {
+  with?: GetCalloutWith[] | undefined;
+}
+
+export async function convertCalloutToData(
+  poll: Poll,
+  member: Member | undefined,
+  opts: ConvertOpts
+): Promise<GetCalloutData> {
+  // fetchPaginatedCallouts prefetches these to reduce the number of queries
+  const hasAnswered =
+    opts.with?.includes(GetCalloutWith.HasAnswered) && member
+      ? poll.hasAnswered !== undefined
+        ? poll.hasAnswered
+        : (await getRepository(PollResponse).count({ poll, member })) > 0
+      : undefined;
+
+  const responseCount =
+    opts.with?.includes(GetCalloutWith.ResponseCount) && member
+      ? poll.responseCount !== undefined
+        ? poll.responseCount
+        : await getRepository(PollResponse).count({ poll })
+      : undefined;
+
+  return {
+    slug: poll.slug,
+    title: poll.title,
+    excerpt: poll.excerpt,
+    image: poll.image,
+    allowUpdate: poll.allowUpdate,
+    allowMultiple: poll.allowMultiple,
+    access: poll.access,
+    status: poll.status,
+    hidden: poll.hidden,
+    starts: poll.starts,
+    expires: poll.expires,
+    ...(hasAnswered !== undefined && { hasAnswered }),
+    ...(responseCount !== undefined && { responseCount }),
+    ...(opts.with?.includes(GetCalloutWith.Form) && {
+      intro: poll.intro,
+      thanksText: poll.thanksText,
+      thanksTitle: poll.thanksTitle,
+      formSchema: poll.formSchema,
+      ...(poll.thanksRedirect && { thanksRedirect: poll.thanksRedirect }),
+      ...(poll.shareTitle && { shareTitle: poll.shareTitle }),
+      ...(poll.shareDescription && {
+        shareDescription: poll.shareDescription
+      })
+    })
+  };
+}
+
+export async function fetchPaginatedCallouts(
+  query: GetCalloutsQuery,
+  member: Member | undefined,
+  opts: ConvertOpts
+): Promise<Paginated<GetCalloutData>> {
+  const scopedQuery = mergeRules(
+    query,
+    !member?.hasPermission("admin") && [
+      // Non-admins can only query for open or ended non-hidden callouts
+      {
+        condition: "OR",
+        rules: [
+          { field: "status", operator: "equal", value: ItemStatus.Open },
+          { field: "status", operator: "equal", value: ItemStatus.Ended }
+        ]
+      },
+      { field: "hidden", operator: "equal", value: false }
+    ]
+  );
+
+  const results = await fetchPaginated(
+    Poll,
+    scopedQuery,
+    {
+      // TODO: add validation errors
+      status: ruleAsQuery,
+      answeredBy: (rule, qb, suffix) => {
+        if (rule.operator !== "equal" || !member) {
+          throw new BadRequestError();
+        }
+
+        const id =
+          rule.value === "me" || rule.value === member.id
+            ? member.id
+            : member.hasPermission("admin")
+            ? rule.value
+            : undefined;
+
+        if (!id) {
+          throw new UnauthorizedError();
+        }
+
+        // TODO: deduplicate with hasAnswered
+        const subQb = createQueryBuilder()
+          .subQuery()
+          .select("pr.pollSlug", "slug")
+          .distinctOn(["pr.pollSlug"])
+          .from(PollResponse, "pr")
+          .where(`pr.memberId = :id${suffix}`)
+          .orderBy("pr.pollSlug");
+
+        qb.where("item.slug IN " + subQb.getQuery());
+
+        return { id };
+      }
+    },
+    (qb) => {
+      if (member && opts.with?.includes(GetCalloutWith.ResponseCount)) {
+        qb.loadRelationCountAndMap("item.responseCount", "item.responses");
+      }
+    }
+  );
+
+  // TODO: this should probably be a LEFT JOIN instead
+  if (
+    member &&
+    results.items.length > 0 &&
+    opts.with?.includes(GetCalloutWith.HasAnswered)
+  ) {
+    const answeredPolls = await createQueryBuilder(PollResponse, "pr")
+      .select("pr.pollSlug", "slug")
+      .distinctOn(["pr.pollSlug"])
+      .where("pr.pollSlug IN (:...slugs) AND pr.memberId = :id", {
+        slugs: results.items.map((item) => item.slug),
+        id: member.id
+      })
+      .orderBy("pr.pollSlug")
+      .getRawMany<{ slug: string }>();
+
+    const answeredSlugs = answeredPolls.map((p) => p.slug);
+
+    for (const item of results.items) {
+      item.hasAnswered = answeredSlugs.includes(item.slug);
+    }
+  }
+
+  return {
+    ...results,
+    items: await Promise.all(
+      results.items.map((item) => convertCalloutToData(item, member, opts))
+    )
+  };
+}
+
+export async function fetchPaginatedCalloutResponses(
+  slug: string,
+  query: GetCalloutResponsesQuery,
+  member: Member
+): Promise<Paginated<GetCalloutResponseData>> {
+  const scopedQuery = mergeRules(query, [
+    { field: "poll", operator: "equal", value: slug },
+    // Member's can only see their own responses
+    !member.hasPermission("admin") && {
+      field: "member",
+      operator: "equal",
+      value: member.id
+    }
+  ]);
+
+  const results = await fetchPaginated(
+    PollResponse,
+    scopedQuery,
+    {
+      member: (rule, qb, suffix, namedWhere) => {
+        qb.where(`item.member ${namedWhere}`);
+        if (rule.value === "me") {
+          return { a: member.id };
+        }
+      }
+    },
+    (qb) => qb.loadAllRelationIds()
+  );
+
+  return {
+    ...results,
+    items: results.items.map((item) => ({
+      member: item.member as unknown as string, // TODO: fix typing
+      answers: item.answers,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt
+    }))
+  };
+}
+
+export * from "./interface";
