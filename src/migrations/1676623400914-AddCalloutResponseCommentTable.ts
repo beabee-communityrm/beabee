5167ef746b511b7240d8aedf8a9177fd68bb456f
Jonas Schatz <jonas.schatz@googlemail.com>
2023-02-17 10:27:09 +0100

    Fix formatting

--- src/migrations/1676623400914-AddCalloutResponseCommentTable.ts
+++ src/migrations/1676623400914-AddCalloutResponseCommentTable.ts
@@ -1,18 +1,31 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddCalloutResponseCommentTable1676623400914 implements MigrationInterface {
-    name = 'AddCalloutResponseCommentTable1676623400914'
+export class AddCalloutResponseCommentTable1676623400914
+  implements MigrationInterface
+{
+  name = "AddCalloutResponseCommentTable1676623400914";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "contact_profile" DROP CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3"`);
-        await queryRunner.query(`ALTER TABLE "contact_profile" ADD CONSTRAINT "UQ_f926258b8d2aa36053b4488d3a3" UNIQUE ("contactId")`);
-        await queryRunner.query(`ALTER TABLE "contact_profile" ADD CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3" FOREIGN KEY ("contactId") REFERENCES "contact"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" DROP CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" ADD CONSTRAINT "UQ_f926258b8d2aa36053b4488d3a3" UNIQUE ("contactId")`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" ADD CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3" FOREIGN KEY ("contactId") REFERENCES "contact"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "contact_profile" DROP CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3"`);
-        await queryRunner.query(`ALTER TABLE "contact_profile" DROP CONSTRAINT "UQ_f926258b8d2aa36053b4488d3a3"`);
-        await queryRunner.query(`ALTER TABLE "contact_profile" ADD CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3" FOREIGN KEY ("contactId") REFERENCES "contact"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" DROP CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" DROP CONSTRAINT "UQ_f926258b8d2aa36053b4488d3a3"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "contact_profile" ADD CONSTRAINT "FK_f926258b8d2aa36053b4488d3a3" FOREIGN KEY ("contactId") REFERENCES "contact"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 }
--- src/models/CalloutResponseComment.ts
+++ src/models/CalloutResponseComment.ts
@@ -1,21 +1,27 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from "typeorm";
 import CalloutResponse from "./CalloutResponse";
 import Contact from "./Contact";
 
 @Entity()
 export default class CalloutReponseComment {
-    @PrimaryGeneratedColumn("uuid")
-    id!: string;
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
 
-    @ManyToOne("Contact")
-    contact!: Contact;
+  @ManyToOne("Contact")
+  contact!: Contact;
 
-    @ManyToOne("CalloutResponse")
-    calloutResponse!: CalloutResponse
+  @ManyToOne("CalloutResponse")
+  calloutResponse!: CalloutResponse;
 
-    @CreateDateColumn()
-    createdAt!: Date;
+  @CreateDateColumn()
+  createdAt!: Date;
 
-    @Column({type: String})
-    text!: String
+  @Column({ type: String })
+  text!: String;
 }
