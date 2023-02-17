c3510425767b8f6f1036d904fb0d2229a73b347f
Jonas Schatz <jonas.schatz@googlemail.com>
2023-02-17 10:10:57 +0100

    Add DB model for callout response comments

---
+++ src/models/CalloutResponseComment.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import CalloutResponse from "./CalloutResponse";
+import Contact from "./Contact";
+
+@Entity()
+export default class CalloutReponseComment {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @ManyToOne("Contact")
+    contact!: Contact;
+
+    @ManyToOne("CalloutResponse")
+    calloutResponse!: CalloutResponse
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @Column({type: String})
+    text!: String
+}
