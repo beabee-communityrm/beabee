578906d2be8c48a6d588cfc2edb9e171573ca288
Will Franklin <will@willpf.co.uk>
2023-03-17 17:18:55 +0000

    Wrap import in a transaction

--- src/tools/database/import.ts
+++ src/tools/database/import.ts
@@ -12,27 +12,29 @@ if (!config.dev) {
 }
 
 db.connect().then(async () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-  });
-
   // File format: first line is SQL, second is params (repeated)
   try {
-    let query = "";
-    for await (const line of rl) {
-      if (query) {
-        console.log("Running " + query.substring(0, 100) + "...");
-        await getManager().query(
-          query,
-          line !== "" ? JSON.parse(line) : undefined
-        );
-        query = "";
-      } else {
-        query = line;
+    await getManager().transaction(async (manager) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+      });
+
+      let query = "";
+      for await (const line of rl) {
+        if (query) {
+          console.log("Running " + query.substring(0, 100) + "...");
+          await manager.query(
+            query,
+            line !== "" ? JSON.parse(line) : undefined
+          );
+          query = "";
+        } else {
+          query = line;
+        }
       }
-    }
+    });
   } catch (err) {
     console.error(err);
   }
