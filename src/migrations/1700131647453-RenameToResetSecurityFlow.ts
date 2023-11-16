362d3763101e6873e189ddc1584d9d089dfab294
Pascal Garber <pascal@artandcode.studio>
2023-11-16 13:34:42 +0100

    fix(typeorm): Do not drop the foreign key

--- src/migrations/1700131647453-RenameToResetSecurityFlow.ts
+++ src/migrations/1700131647453-RenameToResetSecurityFlow.ts
@@ -4,7 +4,7 @@ export class RenameResetPasswordFlowToResetSecurityFlow1700131647453 implements 
     name = 'RenameResetPasswordFlowToResetSecurityFlow1700131647453'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "reset_password_flow" DROP CONSTRAINT "FK_06c9d2d78e460f9a8ecdba5a592"`);
+        // await queryRunner.query(`ALTER TABLE "reset_password_flow" DROP CONSTRAINT "FK_06c9d2d78e460f9a8ecdba5a592"`);
         await queryRunner.query(`DROP TABLE "reset_password_flow"`);
 
         await queryRunner.query(`CREATE TABLE "reset_security_flow" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "date" TIMESTAMP NOT NULL DEFAULT now(), "type" character varying NOT NULL, "contactId" uuid, CONSTRAINT "PK_4b4e6ea02763fa1a496023d5358" PRIMARY KEY ("id"))`);
