fc802908633be5f3eacc56a5d1dc6e0735429bed
Will Franklin <will@willpf.co.uk>
2024-02-23 10:53:04 +0000

    Ensure variant migration runs after callout ID one

--- src/migrations/1708685548000-AddCalloutVariant.ts
+++ src/migrations/1708685548000-AddCalloutVariant.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddCalloutVariant1708534754022 implements MigrationInterface {
-  name = "AddCalloutVariant1708534754022";
+export class AddCalloutVariant1708685548000 implements MigrationInterface {
+  name = "AddCalloutVariant1708685548000";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
