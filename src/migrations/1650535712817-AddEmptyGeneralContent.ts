18d134c1275eda7ed429fd114140dea7e1a5c789
Pascal Garber <pascal@artandcode.studio>
2024-04-19 13:30:01 +0200

    fix(tax-rate): Revert class renaming of db migration files

--- src/migrations/1650535712817-AddEmptyGeneralContent.ts
+++ src/migrations/1650535712817-AddEmptyGeneralContent.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddEmptyContentGeneral1650535712817 implements MigrationInterface {
+export class AddEmptyGeneralContent1650535712817 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO "content" ("id", "data") VALUES ('general', '{}'::jsonb) ON CONFLICT DO NOTHING`
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> { }
 }
--- src/migrations/1651058503997-AddContactsContent.ts
+++ src/migrations/1651058503997-AddContactsContent.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddContentContacts1651058503997 implements MigrationInterface {
+export class AddContactsContent1651058503997 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO "content" ("id", "data") VALUES ('contacts', '{}'::jsonb) ON CONFLICT DO NOTHING`
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> { }
 }
